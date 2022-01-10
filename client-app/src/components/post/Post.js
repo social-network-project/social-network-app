5adde7dd9037b75b8f4b60aaa48f4611d99ff8d2
mtribbs <89516065+mtribbs@users.noreply.github.com>
2022-01-10 11:08:13 +0100

    post

--- client-app/src/components/post/Post.js
+++ client-app/src/components/post/Post.js
@@ -26,41 +26,17 @@ function Post() {
     addPost();
   };
 
-  const [name, setName] = useState("");
-  const [info, setInfo] = useState("");
-  const [bio, setBio] = useState([
-    {
-      bioName: "Name",
-      bioInfo: "Info",
-    },
-  ]);
-
-  const handleSubmitBio = (e) => {
-    console.log("Bio submit");
-  };
-
   return (
-    <div className="App">
-      <div className="form-box">
-        <PostForm
-          subject={subject}
-          setSubject={setSubject}
-          selectedImage={selectedImage}
-          setSelectedImage={setSelectedImage}
-          caption={caption}
-          setCaption={setCaption}
-          handleSubmitPost={handleSubmitPost}
-        />
-      </div>
-      <div className="form-box">
-        <BioForm
-          name={name}
-          setName={setName}
-          info={info}
-          setInfo={setInfo}
-          handleSubmitBio={handleSubmitBio}
-        />
-      </div>
+    <div>
+      <PostForm
+        subject={subject}
+        setSubject={setSubject}
+        selectedImage={selectedImage}
+        setSelectedImage={setSelectedImage}
+        caption={caption}
+        setCaption={setCaption}
+        handleSubmitPost={handleSubmitPost}
+      />
     </div>
   );
 }
