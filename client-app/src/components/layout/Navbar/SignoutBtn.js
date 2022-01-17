becb35c1843036ebfa773dadfe5d2ff5dc83318f
islandskan <sigridureggertsdottir.art@gmail.com>
2022-01-17 21:07:08 +0100

    some tweaking with the button

--- client-app/src/components/layout/Navbar/SignoutBtn.js
+++ client-app/src/components/layout/Navbar/SignoutBtn.js
@@ -9,5 +9,10 @@ export default function SignoutBtn(props) {
     e.preventDefault();
     setSignout(!signout);
   };
-  return <>{/* <Button></Button> */}</>;
+  return (
+    <>
+      {" "}
+      <Button></Button>{" "}
+    </>
+  );
 }
