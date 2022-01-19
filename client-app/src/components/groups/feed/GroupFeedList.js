ae457f60b563bded38089e5223cdf572a4d8dcf7
Dell <dell@DESKTOP-NLH0UUR>
2022-01-19 11:47:03 +0100

    connectedUser object fix

--- client-app/src/components/groups/feed/GroupFeedList.js
+++ client-app/src/components/groups/feed/GroupFeedList.js
@@ -14,7 +14,7 @@ import { v4 as uuid } from "uuid";
 
 export default function GroupFeedList({
   selectedInterest,
-  connectedUser,
+  connectedUserId,
   users,
 }) {
   const [posts, setPosts] = useState([]);
@@ -36,7 +36,7 @@ export default function GroupFeedList({
     e.preventDefault();
     let newComment = {
       id: uuid(),
-      userId: connectedUser,
+      userId: connectedUserId,
       comment: comment,
     };
     let currentPost = posts.find((x) => x.id === idPost);
@@ -66,7 +66,7 @@ export default function GroupFeedList({
   }
   return (
     <>
-      {selectedInterest && selectedInterest.users.find((x) => x === connectedUser) && (
+      {selectedInterest && selectedInterest.users.find((x) => x === connectedUserId) && (
         <>
           <Segment
             textAlign="center"
